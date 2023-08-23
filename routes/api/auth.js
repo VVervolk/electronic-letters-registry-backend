eabb3c3cef4613c84e0d92e44914c4b1a453de36
VVervolk <vervolk1234@gmail.com>
2023-08-23 15:44:31 +0300

    remake after mentor`s check

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,7 +10,7 @@ const ctrl = require("../../controllers/auth");
 router.post("/register", validateBody(schemas.schemaUser), ctrl.register);
 router.post("/login", validateBody(schemas.schemaUser), ctrl.login);
 router.post("/logout", authenticate, ctrl.logout);
-router.post("/current", authenticate, ctrl.current);
+router.get("/current", authenticate, ctrl.current);
 router.patch(
   "/",
   authenticate,
